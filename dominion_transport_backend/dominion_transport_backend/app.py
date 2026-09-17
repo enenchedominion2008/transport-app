22ac81cc39881e2673f43fcde6c2818910149dfd
enenchedominion2008 <enenchedominion@gmail.com>
2026-09-18 00:12:37 +0100

    fully functional work

--- dominion_transport_backend/dominion_transport_backend/app.py
+++ dominion_transport_backend/dominion_transport_backend/app.py
@@ -555,4 +555,4 @@ def admin_action():
 # ------------------------------------------------------------------
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000) .
